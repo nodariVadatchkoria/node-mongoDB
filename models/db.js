e5789c2646440cfd5c7563070560d5ab3c1dc8f6
nodariVadatchkoria <n.vadatchkoria@alte.edu.ge>
2023-07-13 15:40:57 +0400

    update

--- models/db.js
+++ models/db.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose').default;
 
-// mongoose.connect('mongodb://127.0.0.1:27017/studentDB', {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         seCreateIndex: true
-//     },
-//     err => {
-//         if (!err) {
-//             console.log('Connection succeeded')
-//         } else {
-//             console.log('Error in connection' + err)
-//         }
-//     })
 mongoose.connect('mongodb://127.0.0.1:27017/studentDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
